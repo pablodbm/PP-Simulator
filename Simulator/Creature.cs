b69379733858edfe0443f3eac23231ca1b76ab80
pablodbm <65422439+pablodbm@users.noreply.github.com>
2025-01-07 18:00:06 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,23 @@
+﻿namespace Simulator;
+
+public class Creature
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature() { }
+
+    public string Info => $"{Name} [Level: {Level}]";
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi! I am {Name} and I am at level {Level}.");
+    }
+}
+
